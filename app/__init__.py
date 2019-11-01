d10f05a134bd3c9b39dc9664ee79ca4c6faab306
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2019-11-01 14:34:25 +0530

    Adds, pipenv support (#14)
    * Adds, pipenv support
    
    * Removes dead import
    
    * Removes safety check
    
    * Updates Dockerfile
    
    * Renames dto.py to serializer.py
    
    * Adds, .pytest_cache to gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,15 @@ Module to register our Blueprints
 from flask import Blueprint
 from flask_restplus import Api
 
-from .src.controller.ping import api as ping_ns
+from app.src.controller import ping
 
-blueprint = Blueprint("api", __name__) # pylint: disable=invalid-name
+blueprint = Blueprint("api", __name__)
 
-# pylint: disable=invalid-name
 api = Api(
     blueprint,
     title="Flask-RESTplus API server",
-    version="1.0.0",
-    description="REST server for py",
+    version="1.1.0",
+    description="REST server template written in Flask",
 )
 
-api.add_namespace(ping_ns, path="/ping")
+api.add_namespace(ping.api, path="/ping")
--- manage.py
+++ manage.py
@@ -4,29 +4,16 @@ Application entry point
 import os
 
 from flask_cors import CORS
-from flask_script import Manager
 
 from app import blueprint
 from app.src import create_app
 
 
-app = create_app(os.getenv("FLASK_ENV") or "development")  # pylint: disable=invalid-name
+app = create_app(os.getenv("FLASK_ENV") or "development")
 app.register_blueprint(blueprint)
 CORS(app, resources={"/ping/*": {"origins": "*"}})
 app.app_context().push()
 
-manager = Manager(app)  # pylint: disable=invalid-name
-
-
-@manager.command
-def run():
-    """
-    Command to run the server at localhost:5000 for default values.
-
-    $ python manage.py run
-    """
-    app.run()
-
 
 if __name__ == "__main__":
-    manager.run()
+    app.run()
